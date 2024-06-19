91ffd7295a9f6f22e5bb43c77548a9d128d3917b
Lucas Coelho <coelho_faria16@hotmail.com>
2024-06-18 21:25:14 -0300

    initial commit

---
+++ intro.js
@@ -0,0 +1,27 @@
+window.onload = function() {
+    const boxes = document.querySelectorAll('.box');
+    let currentBox = 0;
+
+    function showBox(index) {
+        boxes.forEach((box, i) => {
+            if (i === index) {
+                box.style.opacity = 1;
+                box.style.zIndex = 1;
+            } else {
+                box.style.opacity = 0;
+                box.style.zIndex = 0;
+            }
+        });
+    }
+
+    function cycleBoxes() {
+        showBox(currentBox);
+        currentBox = currentBox + 1;
+        if(currentBox == 5){
+            window.location.pathname = "/index.html"
+        }
+        setTimeout(cycleBoxes, 4000);
+    }
+
+    cycleBoxes();
+}
